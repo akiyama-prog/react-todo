377723cdac5cfb171c8641c09ea792e0824c0f39
akiyama-prog <autumn56y@gmail.com>
2021-11-12 18:54:14 +0900

    完了と未完了のタスクに分ける

--- src/components/App.js
+++ src/components/App.js
@@ -15,15 +15,36 @@ function App() {
         fetchData();
     }, []);
 
+    //未完了のタスクをもつ配列作成
+    const inCompletedList = todoList.filter((todo) => {
+        return !todo.done;
+    })
+    const completedList = todoList.filter((todo) => {
+        return todo.done;
+    })
+
     return (
         <>
             <h1>Todoリスト</h1>
             <textarea></textarea>
             <button>+ add</button>
-
+            <h2>未完了タスク</h2>
             <ul>
-                {todoList.map((todo) => (
-                    <li key={todo.id}>{todo.content}({todo.done ? 'done' : 'not yet'})</li>
+                {inCompletedList.map((todo) => (
+                    <li key={todo.id}>{todo.content}
+                        <button>{todo.done ? '未完了にする' : '完了にする'}</button>
+                        <button>Del</button>
+                    </li>
+                ))}
+            </ul>
+
+            <h2>完了タスク</h2>
+            <ul>
+                {completedList.map((todo) => (
+                    <li key={todo.id}>{todo.content}
+                        <button>{todo.done ? '未完了にする' : '完了にする'}</button>
+                        <button>Del</button>
+                    </li>
                 ))}
             </ul>
         </>
